77c3aa5929c28a9cc123dae0316b185097b6926d
Jane Justiz <janejustiz@gmail.com>
2018-08-10 21:26:53 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,23 +10,7 @@ class Scraper
     binding.pry
   end
 
-  def get_courses
-  end
 
-  def make_courses
-  end
-
-
-  def print_courses
-  self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
 
 Scraper.new.get_page
 
